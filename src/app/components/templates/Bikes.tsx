1d2516c42c65e63d8068087952eb0fe4c61b1493
Vishal <vishal.bhumca21@gmail.com>
2025-06-30 15:19:17 +0530

    fixed bikes

--- src/app/components/templates/Bikes.tsx
+++ src/app/components/templates/Bikes.tsx
@@ -32,12 +32,11 @@ const Bikes = () => {
         <div className=" shadow-2xl w-[92%] relative pb-20 bg-white">
             <div className="flex md:flex-row justify-center flex-col-reverse md:p-8 px-3 py-5 ">
             {/* left */}
-            <motion.div
-            initial={{x:-50,opacity:0}}
+            <motion.div 
+            initial={{x:-100,opacity:0}}
             whileInView={{x:0,opacity:1}}
-            transition={{duration:0.5,delay:1}}
-
-             className="md:w-1/2 w-full max-w-[620px]  md:p-2 relative flex flex-col md:gap-y-6 gap-y-4 ">
+            transition={{duration:0.5}}
+            className="md:w-1/2 w-full max-w-[620px]  md:p-2 relative flex flex-col md:gap-y-6 gap-y-4 ">
                 <HeaderContent primaryContent="Lorem ipsum dolor sit" secondaryContent="Lorem ipsum dolor sit amet" contentwidth="md:w-[300px]" descript="Lorem ipsum dolor sit amet consectetur. Amet sodales sociis facilisis donec dui. Mi porttitor ut aliquam mattis maecenas eget integer in nam. Non nisl iaculis at felis aliquet. Hendrerit tellus at purus lectus."/>
             {/* Feature Items */}
             <div className="md:pt-14  max-w-[594px] space-y-6">
@@ -49,24 +48,28 @@ const Bikes = () => {
                 ))}
             </div>
             {/* CTA */}
-            <motion.div
-            initial={{y:50,opacity:0}}
-            whileInView={{y:0,opacity:1}}
-            transition={{duration:0.8,delay:1}}
+            <div
+            
              className="flex w-full md:justify-start justify-center gap-x-6 absolute md:-bottom-10 -bottom-16">
                 <Button buttonText={`Loerum Ipsum`} buttonStyle={`${inter.className} bg-[#1959AC] rounded-[5px] text-[15px] text-white flex items-center gap-x-2 md:px-3.5 md:py-1.5 px-3 py-1.5`} rightIcon={<FaArrowRight/>}/>
                 <Button buttonText={`123456789`} buttonStyle={`${inter.className} text-[#1959AC]  flex items-center gap-x-2 px-2 py-1`} leftIcon={<BsTelephone/>}/>
-            </motion.div>
+            </div>
             </motion.div>
             {/* Right */}
             <motion.div
             initial={{scale:0,opacity:0}}
             whileInView={{scale:1,opacity:1}}
-            transition={{duration:0.5,delay:0.2}}
-            className="relative md:w-1/2 w-full"
-            >
-            <Image src={BikeImage} alt="bikes" className=" absolute"/>
-            </motion.div>
+            transition={{duration:0.5,delay:0.2}} 
+            className="md:w-1/2 w-full">
+  <Image
+    src={BikeImage}
+    alt="bikes"
+    className="w-full h-auto object-contain"
+    layout="responsive"
+    width={800}   
+    height={600}  
+  />
+</motion.div>
             </div>
             <div className="bg-linear-90 from-[#043898] via-[#079902] to-[#170041] md:w-[96%] w-full md:h-[20px] h-[12px] absolute bottom-0"></div>
         </div>
